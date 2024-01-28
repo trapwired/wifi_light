ebac3b96ef1291911b9a42c9fb9f553273cfc14b
Dominic Weibel <dominic.weibel@digitecgalaxus.ch>
2024-01-28 21:41:41 +0100

    add pifacedigitalio

--- main.py
+++ main.py
@@ -1,28 +1,27 @@
 from flask import Flask
-import piface.pfio as pfio
+import pifacedigitalio
 
 app = Flask(__name__)
-
+pfd = pifacedigitalio.PiFaceDigital()
 
 @app.route('/single', methods=['GET'])
 def one_press():
     print('single')
-    pfio.digital_write(0, 1)
+    pfd.relays[0].value = 1
 
 
 @app.route('/double', methods=['GET'])
 def two_presses():
     print('double')
-    pfio.digital_write(1, 1)
+    pfd.relays[0].value = 2
 
 
 @app.route('/long', methods=['GET'])
 def long_press():
     print('long')
-    pfio.digital_write(1, 0)
-    pfio.digital_write(0, 0)
+    pfd.relays[0].value = 1
+    pfd.relays[0].value = 2
 
 
 if __name__ == '__main__':
-    pfio.init()
     app.run(host='0.0.0.0', threaded=True, port=5000)
